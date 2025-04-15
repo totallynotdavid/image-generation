d03f2767cc4e27a5f09d9fd74a64086d3d0633d9
David Duran <dadch1404@gmail.com>
2025-04-15 15:58:51 -0500

    delete useless comments

--- example/main.ts
+++ example/main.ts
@@ -10,33 +10,25 @@ async function testAllModules() {
   const inputImage7 = "example/input-7.jpg";
   const inputImage8 = "example/input-8.jpg";
 
-  // Test filter modules
   console.log("Testing filter modules...");
 
-  // Blur filter
   const blurredImage = await processImage(inputImage, "blur", 5);
   await Deno.writeFile("example/output/blur.png", blurredImage);
 
-  // Greyscale filter
   const greyscaleImage = await processImage(inputImage, "greyscale");
   await Deno.writeFile("example/output/greyscale.png", greyscaleImage);
 
-  // Invert filter
   const invertedImage = await processImage(inputImage, "invert");
   await Deno.writeFile("example/output/invert.png", invertedImage);
 
-  // Sepia filter
   const sepiaImage = await processImage(inputImage, "sepia");
   await Deno.writeFile("example/output/sepia.png", sepiaImage);
 
-  // Gay filter
   const gayImage = await processImage(inputImage, "gay");
   await Deno.writeFile("example/output/gay.png", gayImage);
 
-  // Test GIF modules
   console.log("Testing GIF modules...");
 
-  // Blink GIF
   const blinkGif = await processImage(
     [
       inputImage,
@@ -52,32 +44,26 @@ async function testAllModules() {
   );
   await Deno.writeFile("example/output/blink.gif", blinkGif);
 
-  // Triggered GIF
   const triggeredGif = await processImage(inputImage, "triggered");
   await Deno.writeFile("example/output/triggered.gif", triggeredGif);
 
-  // Test montage module
   console.log("Testing montage module...");
 
-  // Ad montage
   const adMontage = await processImage(inputImage, "ad");
   await Deno.writeFile("example/output/ad.png", adMontage);
 
   // Test utility modules
   console.log("Testing utility modules...");
 
-  // Circle utility
   const circleImage = await processImage(inputImage, "circle");
   await Deno.writeFile("example/output/circle.png", circleImage);
 
-  // Color utility
   const colorImage = await processImage(inputImage, "color", "#FF0000");
   await Deno.writeFile("example/output/color.png", colorImage);
 
   console.log("All modules tested successfully!");
 }
 
-// Create output directory if it doesn't exist
 try {
   await Deno.mkdir("example/output", { recursive: true });
 } catch (error) {
@@ -86,5 +72,4 @@ try {
   }
 }
 
-// Run the tests
 await testAllModules();
