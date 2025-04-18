ebfa9fe3f9b51d1c19d4837387502210a0c75a43
David Duran <dadch1404@gmail.com>
2025-04-17 21:39:31 -0500

    chore(build): add build_npm script for packaging and update deno.json with build task; modify .gitignore to exclude build directory

---
+++ build_npm.ts
@@ -0,0 +1,36 @@
+import { build, emptyDir } from 'jsr:@deno/dnt';
+
+await emptyDir('./build');
+
+await build({
+    entryPoints: ['./src/index.ts'],
+    importMap: './deno.json',
+    test: false,
+    outDir: './build',
+    shims: {
+        deno: true,
+        timers: true,
+    },
+    package: {
+        name: '@totallynotdavid/image-generation',
+        version: Deno.args[0]?.replace(/^v/, '') ?? '1.0.0',
+        description:
+            'A TypeScript library for image transformations using sharp and built with Deno',
+        license: 'MIT',
+        repository: {
+            type: 'git',
+            url: 'git+https://github.com/totallynotdavid/image-generation.git',
+        },
+        bugs: {
+            url: 'https://github.com/totallynotdavid/image-generation/issues',
+        },
+        devDependencies: {
+            '@types/node': '^20.0.0',
+            '@types/gifencoder': '^2.0.3',
+        },
+    },
+    postBuild() {
+        Deno.copyFileSync('license', 'build/LICENSE');
+        Deno.copyFileSync('readme.md', 'build/README.md');
+    },
+});
